9935cd92d879a4573a7867c6de70c13dbd6d85a2
HromayaHorse <the_pigeon_is_eating@protonmail.com>
2023-08-17 11:28:20 +0300

    rebuild keyboards

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const { Telegraf } = require('telegraf');
 
 const Prediction = require('./api');
@@ -11,10 +10,10 @@ bot.command('start', async ctx => {
     await bot.telegram.sendPhoto(ctx.chat.id, "https://i.pinimg.com/550x/6c/93/a5/6c93a5baf59208f3485b4610e0170582.jpg")
     await bot.telegram.sendMessage(ctx.chat.id, 'Здравствуй путник. Рад тебя видеть в таверне у старого ублюдка Шарлатана. Чем могу помочь?', {
         reply_markup: {
-            inline_keyboard: [
+            resize_keyboard: true,
+            keyboard: [
                 [{  
                         text: "Магический Шар",
-                        callback_data: 'magicball'
                     }
                 ],
 
@@ -25,14 +24,14 @@ bot.command('start', async ctx => {
 });
 
 
-bot.action('magicball', async ctx => {
+bot.hears('Магический Шар', async ctx => {
     await bot.telegram.sendPhoto(ctx.chat.id, "https://img.freepik.com/premium-vector/mystic-ball-with-pixel-art-style_475147-241.jpg")
     await bot.telegram.sendMessage(ctx.chat.id, 'Загадай на что ты хочешь получить ответ и потряси шар', {
         reply_markup: {
-            inline_keyboard: [
+            resize_keyboard: true,
+            keyboard: [
                 [{  
                         text: "Трясти шар",
-                        callback_data: 'shakedball'
                     }
                 ],
 
@@ -41,21 +40,20 @@ bot.action('magicball', async ctx => {
     })
 })
 
-bot.action('shakedball', async ctx => {
+bot.hears('Трясти шар', async ctx => {
     predict = await api.getRandomPrediction()
     await bot.telegram.sendPhoto(ctx.chat.id, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrnASZKf8G60GlDfKTHkqQleWnbTKzBokhja4U48ex_ZLgHNQ_Y8MJvQTGYsWFrKELiGs&usqp=CAU")
     await bot.telegram.sendMessage(ctx.chat.id, JSON.stringify(predict.data[0].prediction))
 
     await bot.telegram.sendMessage(ctx.chat.id, 'Загадай на что ты хочешь получить ответ и потряси шар', {
         reply_markup: {
-            inline_keyboard: [
+            resize_keyboard: true,
+            keyboard: [
                 [{  
                         text: "Трясти шар",
-                        callback_data: 'shakedball'
                     },
                     {
                         text: "Вернуться к ебучему Шарлатану",
-                        callback_data: 'come_back_to_Sharlatan'
                     }
                 ],
 
@@ -64,14 +62,14 @@ bot.action('shakedball', async ctx => {
     })
 });
 
-bot.action('come_back_to_Sharlatan', async ctx => {
+bot.hears('Вернуться к ебучему Шарлатану', async ctx => {
     await bot.telegram.sendPhoto(ctx.chat.id, "https://i.pinimg.com/550x/6c/93/a5/6c93a5baf59208f3485b4610e0170582.jpg")
     await bot.telegram.sendMessage(ctx.chat.id, 'Здравствуйте снова путник. Это снова я, ваш ебучий Шарлатан. Чем могу помочь?', {
         reply_markup: {
-            inline_keyboard: [
+            resize_keyboard: true,
+            keyboard: [
                 [{  
                         text: "Магический Шар",
-                        callback_data: 'magicball'
                     }
                 ],
 
